e3041ea373424b3cd49762728f447395e7357216
Kevsl <ktr2@pm.me>
2024-01-04 20:55:37 +0100

    ez

--- Assets/js/api.js
+++ Assets/js/api.js
@@ -3,9 +3,7 @@ let countLabel = document.querySelector('.countLabel')
 async function displayAllCharacters() {
     const reponse = await fetch('https://rickandmortyapi.com/api/character')
     const characters = await reponse.json()
-
     countLabel.textContent = `Les ${characters.info.count} personnages.`
-
     let results = characters.results
 
     results.map((result) => {

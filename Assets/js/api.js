6c5d9321dd892c22b9e967e7770d61ec6c515a20
Kevsl <ktr2@pm.me>
2024-01-05 08:58:55 +0100

    exercice start

--- Assets/js/api.js
+++ Assets/js/api.js
@@ -1,18 +1,24 @@
 let countLabel = document.querySelector('.countLabel')
 let cardContainer = document.querySelector('.cardContainer')
 let nav = document.querySelector('.nav')
+let errorMessage = document.querySelector('.errorMessage')
+let characters = []
 
-async function displayAllCharacters(url) {
+async function getAllCharacters(url) {
     cardContainer.innerHTML = ''
-    if (!url) url = 'https://rickandmortyapi.com/api/character'
-    const reponse = await fetch(url)
-    const characters = await reponse.json()
+    if (!url) url = 'https://rickandmortyapi.com/api/lsmlqslkmqd'
 
-    characters.results.map((result) => {
-        createCard(result.image, result.name)
-    })
-    createNav(characters.info.prev, characters.info.next)
-    countLabel.textContent = ` Il y a ${characters.info.count} personnages.`
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+
+        const characters = await response.json()
+        displayCharacters(characters)
+    } catch (error) {
+        displayError(error)
+    }
 }
 
 function createCard(image, name) {
@@ -25,10 +31,28 @@ function createCard(image, name) {
 function createNav(prev, next) {
     nav.innerHTML = ''
     if (prev) {
-        nav.innerHTML += `<button class="mr-auto" onclick="displayAllCharacters('${prev}')">Prev</button>`
+        nav.innerHTML += `<button class="mr-auto" onclick="getAllCharacters('${prev}')">Prev</button>`
     }
     if (next) {
-        nav.innerHTML += `<button class="right-0 absolute" onclick="displayAllCharacters('${next}')">Next</button>`
+        nav.innerHTML += `<button class="right-0 absolute" onclick="getAllCharacters('${next}')">Next</button>`
     }
 }
-displayAllCharacters()
+
+function displayCharacters(characters) {
+    countLabel.textContent = ` Il y a ${characters.info.count} personnages.`
+    characters.results.map((result) => {
+        createCard(result.image, result.name)
+    })
+    createNav(characters.info.prev, characters.info.next)
+}
+
+function displayError(error) {
+    switch (error) {
+        case 400:
+            'Page introuvable'
+    }
+
+    errorMessage.innerText = error
+}
+
+getAllCharacters()
